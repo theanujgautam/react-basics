0d4a3a6df5e6cd446eb8cfb61c5671bfa18d0008
theanujgautam <akaygautam.skills@gmail.com>
2023-12-01 05:52:09 +0530

    Creating and Nesting Compononets

---
+++ react-basics/src/App.js
@@ -0,0 +1,11 @@
+import MyButton from './MyButton.js';
+function App() {
+  return (
+    <>
+      <p>This is a paragraph.</p>
+      <MyButton />
+    </>
+  );
+}
+
+export default App;
---
+++ react-basics/src/MyButton.js
@@ -0,0 +1,7 @@
+function MyButton(){
+    return (
+        <button>Buttton</button>
+    );
+}
+
+export default MyButton;
