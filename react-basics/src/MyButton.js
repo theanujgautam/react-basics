f951fa67c8b2b053ad8937667b5b3dd2f04458e9
theanujgautam <akaygautam.skills@gmail.com>
2023-12-01 21:10:19 +0530

    Respondng to events

--- react-basics/src/MyButton.js
+++ react-basics/src/MyButton.js
@@ -1,6 +1,15 @@
+// You can respond to events by declaring event handler functions inside your components.
+
+// onClick={handleClick} has no parentheses at the end! Do not call the event handler function: you only need to pass it down. React will call your event handler when the user clicks the button.
+
 function MyButton(){
+
+    function handleClick() {
+        alert('Hello World');
+    }
+
     return (
-        <button>Buttton</button>
+        <button onClick={handleClick}>Buttton</button>
     );
 }
 
